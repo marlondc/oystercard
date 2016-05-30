230da25699291c42e04617294d3bdde332dec6fe
Stephen Dawes <sdawes@outlook.com>
2016-05-30 15:14:24 +0100

    5. Add #top_up

--- lib/feature_test.rb
+++ lib/feature_test.rb
@@ -1,4 +1,6 @@
-require './oystercard'
+require_relative 'oystercard'  # => true
 
-oystercard = Oystercard.new
-oystercard.balance 
+oystercard = Oystercard.new  # => #<Oystercard:0x007f92bc85b0f0 @balance=0>
+oystercard.balance           # => 0
+oystercard.top_up(10)        # => 10
+oystercard.balance           # => 10
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,15 +1,25 @@
 require 'oystercard'
 
-describe Oystercard do 
+describe Oystercard do
 
   let (:oystercard) { Oystercard.new }
 
-  it "creates new OysterCard" do 
+  it "creates new OysterCard" do
     expect(oystercard).to be_a (Oystercard)
   end
 
-  it "has a balance of zero when first created" do 
+  it "has a balance of zero when first created" do
     expect(oystercard.balance).to eq(0)
-  end	
+  end
+
+  describe "#top_up" do
+
+    it { is_expected.to respond_to(:top_up).with(1).argument }
+
+    it "tops up balance on card" do
+      expect { oystercard.top_up(10) }.to change{ oystercard.balance }.by(10)
+    end
+
+  end
 
 end
