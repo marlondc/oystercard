3387cd992b939a7396acf82a4783e69168187de4
Anna <anna.hollandsmith3@gmail.com>
2016-05-30 16:09:07 +0100

    added further unit test to show the balance method operates properly

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -7,6 +7,10 @@ describe Oystercard do
 
   describe '#top_up' do
   	it { is_expected.to respond_to(:top_up).with(1).argument }
+
+  	it 'can top-up the balance' do
+  		expect{ subject.top_up(1).to change(subject.balance).by 1}
+    end
   end
 
 end
