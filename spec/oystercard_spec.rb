ccdfc4c1f1a9ecda554253bbd594204732f800db
Anna <anna.hollandsmith3@gmail.com>
2016-05-30 16:12:04 +0100

    refactored unit test for balance

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -9,7 +9,7 @@ describe Oystercard do
   	it { is_expected.to respond_to(:top_up).with(1).argument }
 
   	it 'can top-up the balance' do
-  		expect{ subject.top_up(1).to change(subject.balance).by 1}
+  		expect{ subject.top_up 1 }.to change{ subject.balance }.by 1
     end
   end
 
