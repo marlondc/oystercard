d7b4ce530cc0239de1b9f529685e9dd29ad2267a
LukaszGr <lukasz.grychowski@gmail.com>
2016-06-03 09:22:33 +0100

    penalty fare almost working

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,29 +1,37 @@
+require_relative 'journey'
+
 class Oystercard
 
   MAX_BALANCE = 90
-  
+
 
   attr_reader :balance, :entry_station, :exit_station, :journeys
 
-  def initialize
+  def initialize(journey = Journey.new)
     @balance = 0
-    @entry_station = nil
+    @current_journey = journey
+    # @entry_station = nil
     @journeys = {}
   end
 
   def touch_in(station)
-    fail "Error: minimum balance less than minimum fare. Top-up!" if @balance < MIN_FARE
-    @entry_station = station
-    @journeys[@entry_station] = nil
-    @entry_station
+    fail "Error: minimum balance less than minimum fare. Top-up!" if @balance < Journey::MIN_FARE
+    @current_journey.start(station)
+
+
+    # @entry_station = station
+    # @journeys[@entry_station] = nil
+    # @entry_station
 
   end
 
   def touch_out(station)
-    deduct(MIN_FARE)
-    @exit_station = station
-    @journeys[@entry_station] = station
-    @entry_station = nil
+    # deduct(Journey::MIN_FARE)
+    @current_journey.finish(station)
+    deduct(@current_journey.fare)
+    # @exit_station = station
+    # @journeys[@entry_station] = station
+    # @entry_station = nil
   end
 
   def top_up(amount)
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -7,10 +7,6 @@ describe Oystercard do
       expect(subject.balance).to eq(0)
     end
 
-    it "expected to not be in journey" do
-      expect(subject).not_to be_in_journey
-    end
-
     it "has no entry station set" do
       expect(subject.entry_station).to eq nil
     end
@@ -27,22 +23,6 @@ describe Oystercard do
 
   describe "#touch_in" do
 
-    context "when card has sufficient balance" do
-
-      let(:station){ double :station }
-      it "can touch in" do
-        subject.top_up(1)
-        subject.touch_in(station)
-        expect(subject).to be_in_journey
-      end
-
-      it "stores entry station" do
-        subject.top_up(1)
-        subject.touch_in(station)
-        expect(subject.entry_station).to eq station
-      end
-    end
-
     context "when card has insufficient balance" do
       let(:station){ double :station }
       it "raises error if card balance below minimum fare value" do
@@ -56,22 +36,8 @@ describe Oystercard do
     let(:entry_station){ double :station }
     let(:exit_station){ double :station }
 
-    it "can touch out" do
-      subject.top_up(1)
-      subject.touch_in(entry_station)
-      subject.touch_out(exit_station)
-      expect(subject).not_to be_in_journey
-    end
-
     it "deducts min fare from balance on touch out" do
       expect{subject.touch_out(exit_station)}.to change{subject.balance}.by(-1)
-    end
-
-    it "stores exit station" do
-      subject.top_up(1)
-      subject.touch_in(entry_station)
-      subject.touch_out(exit_station)
-      expect(subject.exit_station).to eq exit_station
     end
 
     it "forgets entry station" do
@@ -83,12 +49,12 @@ describe Oystercard do
 
     # let(:journey){double :entry_station, :exit_station}
 
-    it "stores a journey" do
-      subject.top_up(1)
-      subject.touch_in(entry_station)
-      subject.touch_out(exit_station)
-      expect(subject.journeys).to eq ({entry_station => exit_station})
-    end
+    # it "stores a journey" do
+    #   subject.top_up(1)
+    #   subject.touch_in(entry_station)
+    #   subject.touch_out(exit_station)
+    #   expect(subject.journeys).to eq ({entry_station => exit_station})
+    # end
 
   end
 
